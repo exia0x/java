8df67e37a3bb417f5bcdffdb28df86593c6f02dd
Juven Lawrinsen <juvenlauwrinsen7@gmai.com>
2026-02-13 21:59:30 +0700

    hai

---
+++ LatihDataMahasiswa.java
@@ -0,0 +1,16 @@
+/*
+NIM : D1041241044
+NAMA : Juven Lawrinsen
+Program untuk variable
+*/
+
+public class LatihDataMahasiswa {
+  public static void main(String[] args) {
+    long NIM = 2301010001L;
+    String nama = "Budi Santoso";
+    char kelas = 'A';
+    Boolean status = true;
+    double IPK = 3.75;
+    int sks = 72; 
+  }
+}
